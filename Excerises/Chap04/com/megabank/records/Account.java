4a1d68b04e12d8e1e5ecdfbb566279fdff00444f
Tommy Thompson <tomthompson828@gmail.com>
2018-10-29 20:13:52 -0400

    completed chpt 4

--- Excerises/Chap04/com/megabank/records/Account.java
+++ Excerises/Chap04/com/megabank/records/Account.java
@@ -2,6 +2,14 @@ package com.megabank.records;
 
 public class Account {
 
+  // Initial Balance @ 0.0 dollars
+  protected double balance;
+
+  // Returns Balance
+  public double getBalance() {
+    return balance;
+  }
+
 	// Proof that I compiled it right ;)
 	static {
 		System.out.println("Hit Account");
--- Excerises/Chap04/com/megabank/system/Database.java
+++ Excerises/Chap04/com/megabank/system/Database.java
@@ -2,15 +2,38 @@ package com.megabank.system;
 
 import com.megabank.records.Account;
 
-public class Database {
+abstract public class Database {
 
+
+  // Deposit dollar amount into account
+  abstract public void deposit(double amount, Account account);
+
+
+  // Withdraw dollar amount from account
+  abstract public void withdraw(double amount, Account account);
+
+
+  // Get Balance of current Account
+  abstract protected void balance(Account account);
+
+
+  // Transer funds between accounts
+  final void transfer(double amount, Account account1, Account account2) {
+
+    // Withdraw from account 1
+    this.withdraw(amount, account1);
+
+    // Deposit into account 2
+    this.deposit(amount, account2);
+
+  }
+
+
+  // Proof that I compiled it right ;)
 	public static void main(String[] args) {
-		// Proof that I compiled it right ;)
 		Account myAccount = new Account();
-		System.out.println("Hit Account");
+		System.out.println("Hit Database");
 	}
-
-
 
 
 }
