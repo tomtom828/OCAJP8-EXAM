eab2fd3f3e8ac30b19e4c5ba94c29ea0b195c376
Tommy Thompson <tomthompson828@gmail.com>
2018-11-10 00:52:37 -0500

    added array list for 6.1 , needed to check for divisibilty of unknown primes

--- Excerises/Chap06/PE_6.1/PrimesForLoop.java
+++ Excerises/Chap06/PE_6.1/PrimesForLoop.java
@@ -1,28 +1,50 @@
+import java.util.ArrayList;
+
 public class PrimesForLoop {
 
   // Finds all primes smaller than 100
   // Note: I am sure there are more elegant ways to do this, but I'll just brute force it...
 
-  private static int NUMBER_LIMIT = 100;
+  private static int NUMBER_LIMIT = 1000;
 
   // Yeah... these are prime... but this is a Java lesson, not a math lesson...
   private static int[] arrayOfDivisors = {2, 3, 5, 7};
 
+  // ArrayList of solved primes
+  private static ArrayList<Integer> listOfSolvedPrimes = new ArrayList<Integer>();
+
 
   // Return true/false if prime or not, respectively
   private static boolean isPrime(int myNumber) {
+
+    // Zero or One is not prime
+    if (myNumber == 0 || myNumber == 1)
+      return false;
+
     // Test if of the given prime numbers
     for(int i = 0; i <arrayOfDivisors.length; i++) {
-      if (myNumber == arrayOfDivisors[i])
+      if (myNumber == arrayOfDivisors[i]) {
+        listOfSolvedPrimes.add(myNumber);
         return true;
+      }
     }
+
     // If divisble by any of these numbers, then its not prime
     for(int i = 0; i <arrayOfDivisors.length; i++) {
       if (myNumber % arrayOfDivisors[i] == 0)
         return false;
     }
+
+    // Cannot be divisible by another prime (i.e. 13*13 = 169)
+    for(int i = 0; i < listOfSolvedPrimes.size(); i++) {
+      if (myNumber % listOfSolvedPrimes.get(i) == 0)
+        return false;
+    }
+
     // Otherwise, it is prime
+    listOfSolvedPrimes.add(myNumber);
     return true;
+
   }
 
 
@@ -34,6 +56,6 @@ public class PrimesForLoop {
         System.out.println(i);
     }
   }
-  
+
 
 }
